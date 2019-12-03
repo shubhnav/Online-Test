d9ec4393fd006be32170ada1b7976dc3e47b2852
shubhnav <shubhanshuprajapati18@gmail.com>
2019-12-03 23:20:43 +0530

    added
    added

---
+++ client/src/Task.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Task extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(<>Hello from Task</>);
+  }
+}
+export default Task;
