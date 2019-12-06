4502ae6cb8307e5ae92113e6646bd2eb231350a0
shubhnav <shubhanshuprajapati18@gmail.com>
2019-12-06 21:42:30 +0530

    added
    Server added

---
+++ Server/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const router = express.Router();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.json({limit:'5mb'}));
+/*router.all('/', function(req, res) {
+    console.log("called", JSON.stringify(req));
+    res.json("Hello")
+});
+*/
+
+ app.post("/api",function (request,response){
+   try{
+   console.log("request from client",request.body);
+   let data = {
+	"message": "Lol" 
+   };
+   
+   console.log("request from client",data);
+   response.send(JSON.stringify(data));
+  }
+  catch(err){
+	console.log("Error",err);
+}
+ });
+//
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+//app.use('/api', router);
+app.listen(5000,()=>{
+  console.log("server started");
+});
--- client/src/Task.jsx
+++ client/src/Task.jsx
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 class Task extends Component{
   constructor(props){
     super(props);
+    this.state = {
+	message : ""
+    }
+    
   }
+  async componentWillMount(){
+      await fetch("/api",{
+      method: "post",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({"message": "Hello"})
+      }).then( async function(response){
+	let data = await response.json();
+	return data;
+      }).then(data=>{
+		console.log("client==>",data);
+		this.setState({
+			message: data.message
+		})
+	})
+          
+}
   render(){
-    return(<>Hello from Task</>);
+
+    	return(<>Hello from Task  {this.state.message}</>);
+    
+ 
   }
 }
 export default Task;
