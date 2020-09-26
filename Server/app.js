ed0a0f0509f16ee97fbf35791692bd8c5452f24e
shubhnav <shubhanshuprajapati18@gmail.com>
2020-09-26 23:17:17 +0530

    production
    heroku production changes

--- Server/app.js
+++ Server/app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./router");
 const app = express();
+const PORT = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json({limit:'5mb'}));
 app.use("/api",router);
 
-app.listen(5000,()=>{
-  console.log("server started");
-});
+app.listen(PORT);
